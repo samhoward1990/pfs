a435d5a9d4c1919570e87940ba77c59b3e5eceb1
Sam Howard <samhoward1990@gmail.com>
2020-11-18 15:37:30 -0600

    added navbar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Nav, Image, Button } from 'react-bootstrap';
+import { Navbar, Nav, Image, Button } from 'react-bootstrap';
 import './App.css';
 
+import pfsLogo from './images/PFS_Logo-removebg.png';
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav bg="dark">
-
-        </Nav>
+        <div className="nav-div d-flex justify-content-center">
+          <Nav className="bg-dark positioned-nav justify-content-around">
+            <Link to="/"><Button variant="outline-light">Home</Button></Link>
+            <Link to="/about"><Button variant="outline-light">About</Button></Link>
+          </Nav>
+          <Link to="/"><img src={pfsLogo} className="logo" width="150px" height="150px" /></Link>
+        </div>
       </div>
-    </Router>
+    </Router >
   );
 }
 
