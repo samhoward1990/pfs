8fe8391acb15a7c55456f1f22fabf3b8cce10acb
Sam Howard <samhoward1990@gmail.com>
2020-11-20 15:05:16 -0600

    removed tabs from homepage

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, Image, Tabs, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 import loveOfLemons from '../../images/love-of-lemons.jpg';
 
@@ -9,19 +9,12 @@ class Homepage extends React.Component {
             <Container className="positioned-container">
                 <Row>
                     <Col lg="12">
-                        <Tabs defaultActiveKey="discography">
-                            <Tab eventKey="discography" title="Discography">
-                                <Image src={loveOfLemons} width="200px" height="200px" />
-                            </Tab>
-                            <Tab eventKey="Events" title="Events">
 
-                            </Tab>
-                        </Tabs>
                     </Col>
                 </Row>
             </Container>
         )
-    }
-}
+    };
+};
 
 export default Homepage;
